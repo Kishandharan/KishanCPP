05463bac41fbec6e5236336e381097642abf5199
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-10-30 14:54:10 +0400

    Added some stuff

---
+++ 1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    std::vector<std::vector<std::vector<std::string>>> threeDSpace =  {
+        {{"Hello", "World"}, {"I", "Am"}, {"In a", "3d world"}}
+    };
+}
